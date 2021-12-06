94b47d23ff4023197f509f605bae27788467058d
DEVU R KURUP <devukurup199918@gmail.com>
2021-12-06 20:09:40 +0530

    Created category model

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_090542) do
+ActiveRecord::Schema.define(version: 2021_12_06_143603) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -23,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_12_04_090542) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "categories", "users"
 end
